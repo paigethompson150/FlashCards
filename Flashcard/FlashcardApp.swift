d6474fa597bf4870931f1afd67fb8483cefdc285
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-03-15 11:04:32 -0400

    Initial Commit

---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Paige Thompson on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
